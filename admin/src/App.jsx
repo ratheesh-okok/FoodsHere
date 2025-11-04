7aea56c983845da6d1ce21cd028363c00e077648
ratheesh kumar <rathishkumar909@gmail.com>
2025-11-04 17:00:00 +0530

    Fix: stop tracking node_modules

---
+++ admin/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import Sidebar from './components/Sidebar/Sidebar'
+import { Route, Routes } from 'react-router-dom'
+import Orders from './pages/Orders/Orders'
+import List from './pages/List/List'
+import Add from './pages/Add/Add'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+const App = () => {
+
+  const url = "http://localhost:4000"
+
+  return (
+    <div>
+      <ToastContainer/>
+      <Navbar/>
+      <hr />
+      <dav className="app-content">
+        <Sidebar/>
+        <Routes>
+            <Route path = "/add" element={<Add url={url}/>}/>
+            <Route path = "/list" element={<List url={url}/>}/>
+            <Route path = "/orders" element={<Orders url={url}/>}/>
+        </Routes>
+      </dav>
+    </div>
+  )
+}
+
+export default App
