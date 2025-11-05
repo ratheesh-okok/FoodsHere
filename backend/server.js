1f41d5fd57a886fb1f1818592d40c71ceee2d42b
ratheesh kumar <rathishkumar909@gmail.com>
2025-11-05 17:49:15 +0530

    fixed the error

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: process.env.FRONTEND_URL || '*',
+    origin: ['https://frontendfoodshere.netlify.app', 'https://adminfoodshere.netlify.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
